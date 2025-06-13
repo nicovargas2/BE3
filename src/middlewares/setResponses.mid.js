912d04f5c6ead6c8d7bf10c3ebad522bb25333a6
nico vargas <njvnicovargas@gmail.com>
2025-06-13 01:35:11 -0300

    Add swagger and doc

--- src/middlewares/setResponses.mid.js
+++ src/middlewares/setResponses.mid.js
@@ -12,12 +12,11 @@ const setResponses = (req, res, next) => {
     // Definimos cada uno de los casos
     res.json200 = (data) => successResponse(200, data);
     res.json201 = (data) => successResponse(201, data);
-    res.json204 = () => successResponse(204, {});
-    res.json400 = () => errorResponse(errors.client);
-    res.json401 = () => errorResponse(errors.auth);
-    res.json403 = () => errorResponse(errors.forbidden);
-    res.json404 = () => errorResponse(errors.notFound);
-    res.json500 = () => errorResponse(errors.fatal);
+    res.json400 = (message) => errorResponse(message || errors.client);
+    res.json401 = (message) => errorResponse(message || errors.auth);
+    res.json403 = (message) => errorResponse(message || errors.forbidden);
+    res.json404 = (message) => errorResponse(message || errors.notFound);
+    res.json500 = (message) => errorResponse(message || errors.fatal);
     next();
 }
 
